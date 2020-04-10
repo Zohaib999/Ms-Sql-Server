6ba35788a1071222c0cdb091422b4e33041bfaae
Zohaib999 <zohaibahmad90190@gmail.com>
2020-04-10 19:53:00 +0500

    Union Quries

---
+++ 12_Union_Quries.sql
@@ -0,0 +1,31 @@
+select * from tblEmployee
+select * from tblsalary
+
+-- Union
+select Id,Name from tblEmployee
+Union 
+select Id,NAme from tblsalary
+
+--Union All
+select Id,Name from tblEmployee
+Union All
+select Id,NAme from tblsalary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
