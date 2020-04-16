006c106e2808d3ebdb4f1db6e434d6b70a2890ab
Zohaib999 <zohaibahmad90190@gmail.com>
2020-04-16 21:47:12 +0500

    Built_IN_functions

---
+++ 15_Built_IN_functions.sql
@@ -0,0 +1,53 @@
+﻿
+-- Left, Right , Charindex, substring
+
+select Left('Zohaib@gmail.com',6)
+
+select right('Zohaib@gmail.com',10)
+
+select CHARINDEX('@','Zohaibahmad90190@gmail.com')
+select CHARINDEX('@','Zohaib@gmail.com')
+
+select SUBSTRING('Zohaibahmad90190@gmail.com',7,10)
+
+
+
+
+select * from tblEmployee
+
+
+
+
+select Name,Email from tblEmployee
+select Name,ISNULL(SUBSTRING(Email,CHARINDEX('@',Email)+1,LEN(Email)-CHARINDEX('@',Email)),'No Email') 
+from tblEmployee
+
+
+
+select * from tblEmployee 
+
+
+insert into tblEmployee values('bihjvvvvvv',2,32,
+'ivhebihvfebb@ciuwbdcwdcbiwiuwrvn','Lahore')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
