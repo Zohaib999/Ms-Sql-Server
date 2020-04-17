c0e57b15af76e21b697c5666e89f49f4bc1a85ba
Zohaib999 <zohaibahmad90190@gmail.com>
2020-04-17 17:51:21 +0500

    Scalar User derfined function

---
+++ 20_Scalar_UserDefined_function.sql
@@ -0,0 +1,46 @@
+﻿-- Scalar User defined Function
+ 
+-- Funtion to find out age in days
+
+create Function F_tblEmployee(@ID int)
+returns int
+AS
+ Begin
+   Declare @Res int
+     set @Res = @ID*365
+     return @Res
+ End
+
+
+-- Calling Funtion
+select dbo.F_tblEmployee(20) As Age_In_Days
+
+
+
+-- Using it with a table
+select Id,Name,Age,dbo.F_tblEmployee(Age) As [Age in Days]
+from tblEmployee
+
+
+-- We can use where clause
+select Id,Name,Age,dbo.F_tblEmployee(Age) As [Age in Days]
+from tblEmployee 
+where dbo.F_tblEmployee(Age) < 10950
+
+
+-- to Check inside a function
+sp_helptext F_tblEmployee
+
+
+
+
+
+
+
+
+
+
+
+
+
+
