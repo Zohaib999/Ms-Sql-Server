a0ed52b4c457ff142834082429de9514c0969a38
Zohaib999 <zohaibahmad90190@gmail.com>
2020-04-17 14:14:52 +0500

    Diff betweeb CAST and CONVERT

---
+++ 18_Diff_between_CAST_and_Convert.sql
@@ -0,0 +1,63 @@
+﻿-- difference between Cast and Convert 
+
+-- Cast convert the value
+select * from TblDOB
+select Id,DOB,CAST(DOB as nvarchar(15)) AS Converted 
+from TblDOB
+-- We can convert it into Date to get only date
+select Id,DOB,CAST(DOB as Date) AS Converted 
+from TblDOB
+
+-- Group By
+select CAST(DOB as date),Count(Id) from TblDOB
+group by CAST(DOB as date)
+
+
+
+
+
+
+
+
+
+-- Convert 
+select * from TblDOB
+-- same as Cast but have (Styles of date)
+select ID,Dob, Convert(varchar,DOB) 
+from tblDOB
+select ID,Dob, Convert(varchar,DOB,101) 
+from tblDOB
+-- We can Group by 
+select Count(ID),Convert(date,DOB) from TblDOB
+group by Convert(date,DOB)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
